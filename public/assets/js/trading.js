b8cfa7c3ae22cdcda166416efca94351921f0d03
Bashet <a_bashet@yahoo.com>
2018-05-22 04:50:37 +0100

    start and stop button

--- public/assets/js/trading.js
+++ public/assets/js/trading.js
@@ -2,13 +2,27 @@
  * Created by Bashet on 22/05/2018.
  */
 $(function () {
+    var intervalId = null;
     $('#btn_start_scanning').click(function (e) {
         e.preventDefault();
 
-        setInterval(function () {
+        $('#btn_start_scanning').prop('disabled', true);
+        $('#btn_stop_scanning').prop('disabled', false);
+
+        intervalId = setInterval(function () {
             $('#frm_trading').submit();
         }, 5000);
 
+    });
+
+    $('#btn_stop_scanning').click(function (e) {
+        e.preventDefault();
+
+        $('#btn_stop_scanning').prop('disabled', true);
+        $('#btn_start_scanning').prop('disabled', false);
+
+        if (intervalId)
+            clearInterval(intervalId);
     });
 
     $('#frm_trading').submit(function (e) {
