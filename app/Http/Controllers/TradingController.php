397ed936be06e414897fd347f9c1271aef554abe
Bashet <a_bashet@yahoo.com>
2018-05-22 14:35:18 +0100

    take last 5 digit

--- app/Http/Controllers/TradingController.php
+++ app/Http/Controllers/TradingController.php
@@ -61,13 +61,16 @@ class TradingController extends Controller
         $ema_12 = 12;
         $ema_26 = 26;
 
-        $first_row = $records->pluck('4'); // take only closing price and convert them as array
+        $first_row = collect($records->pluck('4')); // take only closing price and convert them as array
+        $first_row = $first_row->map(function ($item) {
+            return substr($item, -5, 5);
+        });
         $second_row = [];
         $third_row = [];
         $macd_signals = collect(); // this method will hold the data as object and will give lot of flexibility to work with.
 
-        $second_row_initial = $records->pluck('4')->take($ema_12)->avg(); // take fist 12 and average them
-        $third_row_initial = $records->pluck('4')->take($ema_26)->avg(); // take fist 26 and average them
+        $second_row_initial = $first_row->take($ema_12)->avg(); // take fist 12 and average them
+        $third_row_initial = $first_row->take($ema_26)->avg(); // take fist 26 and average them
 
         for ($i = 0; $i < 25; $i++){
             if($i == 0){
